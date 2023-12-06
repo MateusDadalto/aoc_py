e85640d00f146898f98a262ee61cc2f0a1d819cf
Mateus Dutra Dadalto <mateusdadalto@gmail.com>
2023-12-05 22:23:27 -0500

    feat: Day 5 part 2
    Holy mother of ranges

--- day_5.py
+++ day_5.py
@@ -12,9 +12,9 @@ def add_to_maps(lines: list[str]):
     source_values = []
 
     for line in lines[1:]:
-        d, s, increment = [int(value) for value in line.split()]
-        dest_values.append((d, d + increment))
-        source_values.append((s, s + increment))
+        d, s, delta = [int(value) for value in line.split()]
+        dest_values.append((d, d + delta))
+        source_values.append((s, s + delta))
 
     m = {
         'source': source,
@@ -24,15 +24,44 @@ def add_to_maps(lines: list[str]):
     }
     maps[source] = m
 
-def map_source(source: str, value: int):
+def map_source(source: str, seed_ranges: list[(int, int)]):
+    """
+        values_range: Tuple of (seed_range_start, delta)
+    """
     m = maps[source]
-
-    for i, (start, end) in enumerate(m['source_values']):
-        if start <= value < end:
-            delta = value - start
-            return m['dest_values'][i][0] + delta, m['destination']
     
-    return value, m['destination']
+    mapped_values = []
+    for i, (src_start, src_end) in enumerate(m['source_values']):
+        destination, destination_end = m['dest_values'][i]
+        j = 0
+        while j < len(seed_ranges):
+            seed_start, seed_delta = seed_ranges[j]
+            # seed starts before src_range and finishes in the middle of the interval
+            if seed_start <= src_start < seed_start + seed_delta <= src_end:
+                mapped_values.append((destination, seed_start + seed_delta - src_start))
+                seed_ranges[j] = (seed_start, src_start - seed_start)
+            # seed starts in the middle of the interval and goes beyond it
+            elif src_start <= seed_start < src_end <= seed_start + seed_delta:
+                mapped_values.append((destination + seed_start - src_start, (destination_end - destination) + (src_start - seed_start)))
+                seed_ranges[j] = (src_end, seed_start + seed_delta - src_end)
+            # seed starts before and ends after the source range
+            elif seed_start <= src_start < src_end <= seed_start + seed_delta:
+                mapped_values.append((destination, destination_end - destination))
+                seed_ranges[j] = (seed_start, src_start - seed_start)
+                seed_ranges.append((src_end, seed_start + seed_delta - src_end))
+            
+            # seed fully contained in range -> map to destination 
+            # remove seed from values to map bc it is fully mapped
+            if src_start <= seed_start < seed_start + seed_delta <= src_end:
+                mapped_values.append((destination + seed_start - src_start, seed_delta))
+                seed_ranges[j] = seed_ranges[-1]
+                seed_ranges.pop()
+            else: 
+                j += 1
+    
+    mapped_values.extend(seed_ranges)
+    
+    return mapped_values, m['destination']
             
 
 
@@ -55,16 +84,11 @@ with open(path, 'r') as file:
     
 
 locations = []
-seed_range = [seeds[i:i+2] for i in range(0, len(seeds), 2)]
-print(seed_range)
-for r in seed_range:
-    print(r)
-    for seed in range(r[0], r[0] + r[1] + 1):
-        source = 'seed'
-        value = seed
-        while source != 'location':
-            value, source = map_source(source, value)
+sr = [tuple(seeds[i:i+2]) for i in range(0, len(seeds), 2)]
+answer = 0
+source = 'seed'
+while source != 'location':
+    sr, source = map_source(source, sr)
 
-        locations.append(value)
-
-print(min(locations))
+answer = min(v[0] for v in sr)
+print(answer)
