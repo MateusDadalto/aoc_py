f469b1b4159084daae3f24c4475d0081e70f5275
Mateus Dutra Dadalto <mateusdadalto@gmail.com>
2023-12-08 00:33:00 -0500

    feat: day 8 part 2
    of course this is going to take forever to run
    
    going to see if cycle detection saves me now

--- day_8.py
+++ day_8.py
@@ -1,6 +1,5 @@
 from itertools import cycle
 
-
 path = "day_8.txt"
 # path = "test.txt"
 
@@ -16,12 +15,12 @@ for node in n.split('\n'):
     right = node[12:15]
     nodes[name] = {'R': right, 'L': left}
 
-current = 'AAA'
+current: list[str] = [a for a in nodes.keys() if a.endswith('A')]
 counter = 0
 instructions = cycle(instructions)
-while(current != 'ZZZ'):
+while(not all(z.endswith('Z') for z in current)):
     i = next(instructions)
-    current = nodes[current][i]
+    current = [nodes[c][i] for c in current]
     counter += 1
 
 print(counter)
