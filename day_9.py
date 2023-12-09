1c3a9881c54198cc77e53ebd58638a28cd0599bf
Mateus Dutra Dadalto <mateusdadalto@gmail.com>
2023-12-09 00:16:31 -0500

    feat: Day 9 part 1
    look at me using recursion like an academic

---
+++ day_9.py
@@ -0,0 +1,19 @@
+path = "day_9.txt"
+# path = "test.txt"
+
+sequences = []
+with open(path, 'r') as file:
+    for line in file:
+        sequences.append([int(x) for x in line.split()])
+
+
+def diff(seq: list[int]):
+    return [seq[i+1] -  seq[i] for i in range(len(seq) - 1)]
+
+def predict(seq: list[int]):
+    if any([i != 0 for i in seq]):
+        return seq[-1] + predict(diff(seq))
+    else:
+        return 0
+
+print(sum([predict(s) for s in sequences]))
