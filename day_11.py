49b8fa48a69c5c17bd4b326b9f28e00956029e12
Mateus Dutra Dadalto <mateusdadalto@gmail.com>
2023-12-11 00:57:14 -0500

    feat: Day 11 part 2
    Told you part 1 was overengineered as fk

--- day_11.py
+++ day_11.py
@@ -3,40 +3,35 @@ from itertools import permutations
 path = "day_11.txt"
 # path = "test.txt"
 
-EMPTY_SPACE = '.'
-
 with open(path, 'r') as file:
     grid = [[c for c in line.strip()] for line in file]
     
 # lets expand SPACE!
 max_col = len(grid[0])
 max_row = len(grid)
-
+galaxies = [(i,j) for i in range(max_row) for j in range(max_col) if grid[i][j] == '#']
 columns = [[line[j] for line in grid] for j in range(max_col)]
 col_has_galaxy = [any(e != '.' for e in col) for col in columns]
 line_has_galaxy = [any(e != '.' for e in line) for line in grid]
-# print(line_has_galaxy)
+expansion_factor = 1_000_000
 
+# print(col_has_galaxy)
+for i,glxy in enumerate(galaxies):
+    col_expansion = col_has_galaxy[0:glxy[1]].count(False) * (expansion_factor - 1)
+    row_expansion = line_has_galaxy[0:glxy[0]].count(False) * (expansion_factor - 1)
 
-expanded = []
-for i in range(max_row):
-    space = []
-    for j in range(max_col):
-        space.append(grid[i][j]) if col_has_galaxy[j] else space.extend([EMPTY_SPACE, EMPTY_SPACE])
-    
-    expanded.append(space) if line_has_galaxy[i] else expanded.extend([space, space])
+    galaxies[i] = (glxy[0] + row_expansion, glxy[1] + col_expansion)
 
-max_col = len(expanded[0])
-max_row = len(expanded)
+    # print(glxy, galaxies[i], sep=" --- ")
 
-galaxies = [(i,j) for j in range(max_col) for i in range(max_row) if expanded[i][j] == '#']
 # print(galaxies)
 
 distances = 0
 for g1,g2 in set(permutations(galaxies, 2)):
     distances += abs(g1[0] - g2[0]) + abs(g1[1] - g2[1])
 
-print(distances/2)
+print(distances//2)
+
 # for i,line in enumerate(expanded):
 #     print("\n", end='')
 #     for j,c in enumerate(line):
