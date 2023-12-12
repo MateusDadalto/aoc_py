5ce97b370acab8fec1601e88e75afa7ad9678b15
Mateus Dutra Dadalto <mateusdadalto@gmail.com>
2023-12-12 00:58:01 -0500

    feat: Day 12 part 1
    brute force for the win

--- 2023/day_12.py
+++ 2023/day_12.py
@@ -1,6 +1,30 @@
+import re
+from itertools import permutations, product, combinations, combinations_with_replacement
+
 path = "day_12.txt"
 # path = "test.txt"
 
-with open(path, 'r') as file:
-    grid = [[c for c in line.strip()] for line in file]
+def validate_line(line, dist):
+    matches = re.findall(r'#+', line)
+    if len(matches) != len(dist):
+        return False
     
+    return not any(len(matches[i]) != dist[i] for i in range(len(matches)))
+
+r = 0
+options = { '?': ['.', '#'] }
+
+with open(path, 'r') as file:
+    for line in file:
+        line = line.strip()
+        springs, dist = line.split()
+        springs = [c for c in springs]
+        dist = [int(d) for d in dist.split(',')]
+        questions = [i for i in range(len(springs)) if springs[i] == '?']
+        # print(len(questions))
+
+        for possibility in product(*[options.get(c, [c]) for c in springs]):
+            # print(possibility)
+            r += 1 if validate_line(''.join(possibility), dist) else 0
+
+print(r)
