89e333ab924f03f214eb21cfda829fa39bf5e566
Mateus Dutra Dadalto <mateusdadalto@gmail.com>
2023-12-13 01:32:30 -0500

    feat: Day 13 part 2
    See, if I had not shot myself in the foot this would be a decent day

--- 2023/day_13.py
+++ 2023/day_13.py
@@ -5,28 +5,31 @@ path = "day_13.txt"
 with open(path, 'r') as file:
     blocks = [lines.split() for lines in file.read().split('\n\n')]
 
-def compare_mirrored(line, i, j, distance):
-    for k in range(0, distance):
-        
-        if line[i-k] != line[j + k]:
+def compare_mirrored(block, i, j, distance, smudged):
+    for k in range(1, distance):
+        differences = sum([1 for a,b in zip(block[i-k], block[j + k]) if a != b])
+        if not smudged and differences == 1:
+            smudged = True
+        elif smudged and differences > 0:
             return False
 
-    return True                
+    return smudged              
 
 def find_reflection(block:list[str]):
     for i in range(len(block) - 1):
         j = i+1
-        if block[i] == block[j]:
+        smudged = False
+        differences = sum([1 for a,b in zip(block[i], block[j]) if a != b])
+        smudged = differences >= 1
+        if differences <= 1:
             distance = min(j, len(block) - j)
-            if compare_mirrored(block, i, j, distance):
+            if compare_mirrored(block, i, j, distance, smudged):
                 return (i, j, distance)
     
     return []
 
 column_grids = [[list(row) for row in zip(*b)] for b in blocks]
-# print(column_grids)
 a = [find_reflection(b) for b in column_grids]
 b = [find_reflection(b) for b in blocks]
-# print(a, b)
 answer =  sum([t[1] for t in a if len(t) > 0]) + sum([t[1]*100 for t in b if len(t) > 0])
 print(answer)
