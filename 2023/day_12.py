f53cdda94f5c0aa9581dd192ee54a9895a3d2e88
Mateus Dutra Dadalto <mateusdadalto@gmail.com>
2023-12-12 20:56:28 -0500

    feat: Day 12 part 2
    Got this one from reddit, rewriting it myself to understand it
    
    Dynamic programming is something that I have to improve, for sure

--- 2023/day_12.py
+++ 2023/day_12.py
@@ -1,35 +1,57 @@
-import re
-from itertools import permutations, product, combinations, combinations_with_replacement
+from functools import cache
+
 
 path = "day_12.txt"
 path = "test.txt"
 
-def validate_line(line, dist):
-    matches = re.findall(r'#+', line)
-    if len(matches) != len(dist):
-        return False
+def f(springs, broken_groups):
+    """ f is a wrapper function to cache the relevant dp part """
     
-    return not any(len(matches[i]) != dist[i] for i in range(len(matches)))
+    @cache
+    def dp(i, group_i):
+        valid_possibilities = 0
+        # when we get to the end of the string
+        if i >= len(springs):
+            # return 1 if the group index is the last one of the groups
+            # meaning that we are in the end of the string and every group was counted
+            return 1 if group_i == len(broken_groups) else 0
+        
+        # if the character is a dot or ? lets check the next character for the same group
+        if springs[i] in ['.', '?']:
+            valid_possibilities = dp(i+1, group_i)
+        
+        # Check if current group fits in this configuration
+        # if it fits, move to next index after the group and next group
+        # fit conditions:
+        # if character is # or ? and we have a valid group index 
+        # AND
+        # is a valid group range
+        #   (current index + group_size is smaller than springs string and there are no '.' in this group space)
+        # AND
+        # group ends after the group range (either end of string or ./?)
+        if springs[i] in ['#', '?'] and group_i < len(broken_groups) and \
+            (i+broken_groups[group_i] <= len(springs) and '.' not in springs[i: i+broken_groups[group_i]]) and \
+            (i+broken_groups[group_i] == len(springs) or springs[i+broken_groups[group_i]] != '#'):
 
-r = 0
-options = { '?': ['.', '#'] }
+            valid_possibilities += dp(i+broken_groups[group_i] + 1, group_i + 1)
+
+        return valid_possibilities
+    
+    return dp(0,0)
+
+answer = 0
 
 with open(path, 'r') as file:
+    lines = []
     for line in file:
         line = line.strip()
         springs, dist = line.split()
-        # unfold
-        springs = ((springs +'?') * 5)[:-1]
-        print(springs)
-        springs = [c for c in springs]
-        # unfold
-        dist = [int(d) for d in dist.split(',')] * 5
-        print(dist)
-        questions = [i for i in range(len(springs)) if springs[i] == '?']
-        # print(len(questions))
+        springs = '?'.join([springs]*5)
+        # print(springs)
+        broken_groups = [int(d) for d in dist.split(',')] * 5
+        # print(broken_groups)
+        a = f(springs, broken_groups)
+        # print(springs, broken_groups, a, sep='   ')
+        answer += a
 
-        for possibility in product(*[options.get(c, [c]) for c in springs]):
-            # print(possibility)
-            r += 1 if validate_line(''.join(possibility), dist) else 0
-
-print(r)
+print(answer)
