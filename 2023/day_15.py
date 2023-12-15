f66147a076ca7500750ae10a1bd008d5ba6e9d5f
Mateus Dutra Dadalto <mateusdadalto@gmail.com>
2023-12-15 00:57:50 -0500

    feat: Day 15 part 2
    Not sure if I followed the instructions...

--- 2023/day_15.py
+++ 2023/day_15.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import OrderedDict
 
 
 path = "day_15.txt"
@@ -17,5 +17,28 @@ def my_hash(s: str):
     
     return current
 
-answer = sum([my_hash(s) for s in seq])
-print(answer)
+boxes = [OrderedDict() for _ in range(256)]
+for s in seq:
+    label = ''
+    i = 0
+    while s[i].isalpha():
+        label += s[i]
+        i += 1
+    
+    symbol = s[i]
+    box = my_hash(label)
+
+    if symbol == '-' and label in boxes[box]:
+        boxes[box].pop(label)
+    elif symbol == '=':
+        number = int(s[-1])
+        boxes[box][label] = number
+    
+    
+answer = [(i, b) for i,b in enumerate(boxes) if len(b.keys()) > 0]
+sum = 0
+for i,b in answer:
+    for j, lens in enumerate(b.values()):
+        sum += (i+1) * (j+1) * lens
+# answer = sum([my_hash(s) for s in seq])
+print(sum)
