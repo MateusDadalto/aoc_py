bd514c970e0c375147decd410d3dfde8ee6afcf1
Mateus Dutra Dadalto <mateusdadalto@gmail.com>
2023-12-18 03:07:21 -0500

    feat: Day 18 part 2
    OKAY, I deduced the fking solution before, by myself. The only mistake was the fking perimeter
    
    I'm so annoyed, it is unbearable. Shoelace formula my ass

--- 2023/day_18.py
+++ 2023/day_18.py
@@ -2,12 +2,6 @@ path = "day_18.txt"
 # path = "test.txt"
 
 map_d = ['R', 'D', 'L', 'U']
-directions = {
-    'L': (0, -1),
-    'D': (1, 0),
-    'R': (0, 1),
-    'U': (-1, 0)
-}
 
 with open(path, 'r') as file:
     lines = [line.strip() for line in file]
@@ -18,26 +12,18 @@ def parse_line(l):
     n = int('0'+color[2:-2], 16)
     n = int(n)
 
-    return n, d
+    return n if d in 'RD' else -n
 
-vertices = [(0,0)]
+x = 0
+y = 0
+area = 0
 perimeter = 0
-for l in lines:
-    n, d = parse_line(l)
-    x = vertices[-1]
-    i,j = directions[d]
-
-    perimeter += n
-    vertices.append((x[0] + (i*n), x[1]+ (j*n)))
-
-s = 0
-for x1, x2 in zip(vertices, vertices[1:]):
-    s += (x1[1] + x2[1]) * (x1[0] - x2[0])
-
-
-print(abs(s/2) + perimeter/2 + 1)
+for horizontal, vertical in zip(lines[::2], lines[1::2]):
+    x_n = parse_line(horizontal)
+    y_n = parse_line(vertical)
+    x += x_n
+    y += y_n
+    area += x*y_n
+    perimeter += abs(x_n) + abs(y_n)
     
-    
-    
-
-    
+print(abs(area) + perimeter//2 + 1)
