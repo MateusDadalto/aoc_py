db5507fe9f6dc3b87a3785dfa6e7f504c8dfc71c
Mateus Dutra Dadalto <mateusdadalto@gmail.com>
2023-12-18 03:01:34 -0500

    feat: Day 18 part 2
    fkn shoelace formula and perimeter that has a dimension

--- 2023/day_18.py
+++ 2023/day_18.py
@@ -1,8 +1,7 @@
 path = "day_18.txt"
 # path = "test.txt"
 
-perimeter = [(0, 0)]
-
+map_d = ['R', 'D', 'L', 'U']
 directions = {
     'L': (0, -1),
     'D': (1, 0),
@@ -10,67 +9,35 @@ directions = {
     'U': (-1, 0)
 }
 
-min_col = 2**32
-max_col = 0
-min_row = 2**32
-max_row = 0
 with open(path, 'r') as file:
-    for line in file:
-        line = line.strip()
+    lines = [line.strip() for line in file]
 
-        d, n, color = line.split()
-        color = color[1:-1]
+def parse_line(l):
+    d, n, color = l.split()
+    d = map_d[int(color[-2:-1])]
+    n = int('0'+color[2:-2], 16)
+    n = int(n)
 
-        for _ in range(int(n)):
-            i, j = perimeter[-1]
-            x, y = directions[d]
-            new_i = i+x
-            new_j = j+y
+    return n, d
 
-            min_col = min(new_j, min_col) 
-            max_col = max(new_j, max_col) 
-            min_row = min(new_i, min_row)
-            max_row = max(new_i, max_row)
+vertices = [(0,0)]
+perimeter = 0
+for l in lines:
+    n, d = parse_line(l)
+    x = vertices[-1]
+    i,j = directions[d]
 
-            perimeter.append((i+x, j+y))
+    perimeter += n
+    vertices.append((x[0] + (i*n), x[1]+ (j*n)))
 
-assert perimeter[0] == perimeter[-1]
-min_col -= 2
-max_col += 2
-min_row -= 2
-max_row += 2
-p = set(perimeter)
-area = 0
-# flood fill outiside
-total_area = ((max_row + 1) - (min_row)) * ((max_col + 1)-(min_col))
+s = 0
+for x1, x2 in zip(vertices, vertices[1:]):
+    s += (x1[1] + x2[1]) * (x1[0] - x2[0])
 
-empty = [(min_row, min_col)]
-counted = set()
-while len(empty) > 0:
-    i,j = empty.pop()
 
-    if (i,j) in counted:
-        continue
-
-    counted.add((i,j))
-    for x, y in directions.values():
-        new_i = x+i
-        new_j = y+j
-        # I'll go crazy, I've spend HALF AN HOUR with a bug in my number bc this SHIT had a typo in line 64
-        # min_row <= new_j <= max_col instead of min_col <= new_j <= max_col
-        if (new_i, new_j) not in p and \
-            min_row <= new_i <= max_row and \
-            min_col <= new_j <= max_col:
-
-            empty.append((new_i, new_j))
+print(abs(s/2) + perimeter/2 + 1)
     
-# for i in range(min_row, max_row + 1):
-#     print("\n", end='')
+    
+    
 
-#     for j in range(min_col, max_col + 1):
-#         if (i,j) in counted:
-#             print('.', end='')
-#         else:
-#             print('#', end='')
-
-print(total_area - len(counted))
+    
