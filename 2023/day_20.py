4f755ad3308a75950e7822f5a37df96dc5f5414c
Mateus Dutra Dadalto <mateusdadalto@gmail.com>
2023-12-20 01:24:32 -0500

    feat: Day 20 part 1
    This one felt like work, parsing nonsense stuf from here to there

---
+++ 2023/day_20.py
@@ -0,0 +1,128 @@
+from collections import deque
+
+
+path = "day_20.txt"
+# path = "test.txt"
+
+LOW = 0
+HIGH = 1
+BROADCASTER = 'broadcaster'
+FLIP_FLOP = '%'
+CONJUNCTION = '&'
+BUTTON = 'button'
+
+class FlipFlop:
+    def __init__(self, connections):
+        self.state = False
+        self.connections = connections
+
+    def __str__(self) -> str:
+        return f'state: {self.state}, connections: {self.connections}'
+
+    def __output(self):
+        return HIGH if self.state else LOW
+
+    def receive(self, source, pulse):
+        if pulse == LOW:
+            self.state = not self.state
+            return self.__output()
+        
+        return None
+
+class Conjunction:
+    def __init__(self, connections):
+        self.state = {}
+        self.connections = connections
+
+    def __str__(self) -> str:
+        return f'state: {self.state}, connections: {self.connections}'
+
+    def __output(self):
+        return LOW if all([p == HIGH for p in self.state.values()]) else HIGH
+        
+    def add_input(self, input):
+        self.state[input] = LOW
+        
+    def receive(self, source, pulse):
+        self.state[source] = pulse
+
+        return self.__output()
+
+class Broadcaster:
+    def __init__(self, connections):
+        self.connections = connections
+
+    def __str__(self) -> str:
+        return f'connections: {self.connections}'
+    
+    def receive(self, source, pulse):
+        return pulse
+
+class Button:
+    def __init__(self) -> None:
+        self.connections = [BROADCASTER]
+
+    def receive(self, source, pulse):
+        return LOW
+
+class Output:
+    def __init__(self) -> None:
+        self.connections = []
+    
+    def receive(self, source, pulse):
+        return None
+
+modules = {BUTTON: Button()}
+conjunctions = set()
+flip_flops = set()
+with open(path, 'r') as file:
+    
+    for line in file:
+        line = line.strip()
+        kind, connections = line.split(' -> ')
+        kind = kind.strip()
+        connections = [s.strip() for s in connections.split(',')]
+
+        if kind.startswith(BROADCASTER):
+            modules[BROADCASTER] = Broadcaster(connections)
+            continue
+
+        name = kind[1:]
+
+        if kind.startswith(FLIP_FLOP):
+            modules[name] = FlipFlop(connections)
+            flip_flops.add(name)
+        elif kind.startswith(CONJUNCTION):
+            modules[name] = Conjunction(connections)
+            conjunctions.add(name)
+        else:
+            assert False, f"All cases should be covered. {line}" 
+
+for m in modules:
+    for c in modules[m].connections:
+        if c in conjunctions:
+            modules[c].add_input(m)
+
+
+low_total = 0
+high_total = 0
+
+for _ in range(1000):
+    q = deque([(BUTTON, None, None)])
+
+    while len(q) > 0:
+        m, source, pulse = q.popleft()
+
+        module = modules.get(m, Output())
+        output = module.receive(source, pulse)
+        if output == None:
+            continue
+
+        if output == HIGH:
+            high_total += len(module.connections)
+        elif output == LOW:
+            low_total += len(module.connections)
+        for c in module.connections:
+            q.append((c, m, output))
+
+print(low_total, high_total, low_total * high_total)
