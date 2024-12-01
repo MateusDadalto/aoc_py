bf08ddb0a512d013aa1c5741c33d977d3ba61500
Mateus D Dadalto <mateusdadalto@gmail.com>
2024-12-01 00:29:24 -0500

    refactor(2024): improve parsing and code readability

--- 2024/day_01.py
+++ 2024/day_01.py
@@ -1,47 +1,30 @@
 path = "day_01.txt"
 
 with open("day_01.txt") as f:
-    a = f.readlines()
+    lines = f.readlines()
 
-    pairs = []
-    for l in a:
-        numbers = []
-        acc = ""
-        for c in l:
-            if c.isnumeric():
-                acc += c
-            elif len(acc) > 0:
-                numbers.append(int(acc))
-                acc = ""
-        
-        pairs.append(numbers)
-    
-    x = [i for i,j in pairs]
-    y = [j for i,j in pairs]
+left_col = []
+right_col = []
 
-    # x.sort()
-    # y.sort()
+for line in lines:
+    l,result_1 = line.split()
+    left_col.append(int(l))
+    right_col.append(int(result_1))
 
-r = 0
 
-# 1
-# for i,j in zip(x,y):
-#     r += abs(i-j)
+result_1 = 0
 
-# print(r)
+left_col.sort()
+right_col.sort()
+
+for i,j in zip(left_col,right_col):
+     result_1 += abs(i-j)
+
+print("Part 1:", result_1)
 
 # 2
-m = []
-for i in x:
-    m.append(i*y.count(i))
-    # print(y.count(i))
+similarity = []
+for i in left_col:
+    similarity.append(i*right_col.count(i))
 
-# print(m)
-
-n = []
-for j in y:
-    n.append(j*x.count(j))
-
-# print(n)
-
-print(sum(m))
+print("Part 2:", sum(similarity))
