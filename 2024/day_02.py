a014c232670dafc388915438a02f92318423863f
Mateus D Dadalto <mateusdadalto@gmail.com>
2024-12-02 00:38:07 -0500

    feat(2024): day 2 part 2
    shitty solution

--- 2024/day_02.py
+++ 2024/day_02.py
@@ -1,7 +1,21 @@
 path = "day_02.txt"
 # path = "test.txt"
 
+def is_safe(arr):
+    direction = None
+    for i in range(len(arr) - 1):
+        a = int(arr[i]) - int(arr[i+1])
 
+        if 1 <= abs(a) <=3:
+            if a < 0 and direction != "ASC":
+                direction = "DESC"
+            elif a > 0 and direction != "DESC":
+                direction = "ASC"
+            else:
+                return False, i
+        else:
+            return False, i            
+    return True, i
 
 with open(path) as f:
     lines = f.readlines()
@@ -10,24 +24,25 @@ safe = 0
 for l in lines:
     ns = l.split()
 
-    direction = None
-    is_safe = True
-    for i in range(len(ns) - 1):
-        
-        a = int(ns[i]) - int(ns[i+1])
+    error = None
+    r,i = is_safe(ns)
 
-        if 1 <= abs(a) <=3:
-            if a < 0 and direction != "ASC":
-                direction = "DESC"
-            elif a > 0 and direction != "DESC" and 1 <= abs(a) <=3:
-                direction = "ASC"
-            else:
-                is_safe = False
-        else:
-            is_safe = False
-
-    if is_safe:
+    if r:
         safe += 1
+    else:
+        damp1, y = is_safe([ns[x] for x in range(len(ns)) if x != i])
+        damp2, y = is_safe([ns[x] for x in range(len(ns)) if x != i+1])
+        damp3, y = is_safe([ns[x] for x in range(len(ns)) if x != i-1])
+
+        if damp1:
+            print("safe damp: ",[ns[x] for x in range(len(ns)) if x != i], l)
+            safe += 1
+        elif damp2:
+            print("safe damp: ",[ns[x] for x in range(len(ns)) if x != i], l)
+            safe += 1
+        elif damp3:
+            print("safe damp: ",[ns[x] for x in range(len(ns)) if x != i-1], l)
+            safe += 1
 
 print(safe)
         
