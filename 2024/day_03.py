77fc16f7349d29a9506274f699ec3acef28c1617
Mateus D Dadalto <mateusdadalto@gmail.com>
2024-12-03 01:03:14 -0500

    feat(2024): day 3 part 2
    logic being logical

--- 2024/day_03.py
+++ 2024/day_03.py
@@ -7,18 +7,29 @@ def parse_mul(s:str):
     first_number = s[s.index("(")+1:s.index(",")]
     second_number = s[s.index(",")+1:-1]
 
-    # print(first_number)
-    # print(second_number)
     return int(first_number) * int(second_number)
 
+def start_disabled(s:str):
+    return s.startswith(s.split("don't()")[0])
+
 with open(path) as f:
-    lines = f.readlines()
+    lines = f.read()
 
 total = 0
 
-for line in lines:
-    for i in re.findall(r"mul\(\d+,\d+\)", line):
-        
-        total += parse_mul(i)
+toggle_sections = []
+dos = lines.split("do()")
+do_then_dont = [i.split("don't()") for i in dos]
+for do in do_then_dont:
+    status = True
+    for section in do:
+        # print(section, status)
+        if status:
+            for i in re.findall(r"mul\(\d+,\d+\)", section):
+                if status:
+                    total += parse_mul(i)
+    
+        status = False
+    
 
-print("p1: ", total)
+print("p2: ", total)
