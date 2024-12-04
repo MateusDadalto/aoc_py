d4747a1aa096a609466b2047ad61227ffc602a0a
Mateus D Dadalto <mateusdadalto@gmail.com>
2024-12-04 00:57:19 -0500

    feat(2024): day 4 part 2
    grids... I won't be able to read this tomorrow

--- 2024/day_04.py
+++ 2024/day_04.py
@@ -1,3 +1,4 @@
+from collections import Counter
 path = "day_04.txt"
 # path = "test.txt"
 
@@ -36,3 +37,30 @@ for i in range(len(lines)):
             total += count_xmas(lines, i, j, 0)
 
 print(total)
+
+### part 2
+
+def check_diagonal_mas(lines, i, j):
+    diag = [(0,0), (1,1), (2,2)]
+    
+    return all([ch in [lines[i+x][j+y] for x,y in diag] for ch in ['M', 'A', 'S']])
+
+def x_mas(lines, i, j):
+    corners = [(0,0), (2,0), (0,2), (2,2)]
+    center =  (1,1)
+    
+    corner_letters = Counter([lines[i+x][j+y] for x,y in corners])
+    if corner_letters.get('M', 0) == 2 and corner_letters.get('S', 0) == 2:
+        return check_diagonal_mas(lines, i, j)
+    
+    return False
+    
+p2 = 0
+for i in range(ROW_SIZE - 2):
+    for j in range(COL_SIZE - 2):
+        center = lines[i+1][j+1]
+        
+        if center == "A" and x_mas(lines, i, j):
+            p2 += 1
+            
+print(p2)
