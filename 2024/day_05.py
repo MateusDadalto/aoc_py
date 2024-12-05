a51b007ecf6de2971777c77960da2d09d45feae1
Mateus D Dadalto <mateusdadalto@gmail.com>
2024-12-05 01:02:26 -0500

    feat(2024): day 5 part 2
    What a fucking bad choice of words for variables
    
    1000 bugs because of that

--- 2024/day_05.py
+++ 2024/day_05.py
@@ -25,24 +25,69 @@ for order in orders:
     rules[x] = rule_x
     rules[y] = rule_y
     
-p1 = 0
 
-for i in instructions:
-    numbers = [int(j) for j in i.split(',')]
-    
-    valid = True
+def validate_rules(numbers, rules):
     for n in range(len(numbers)):
         before = numbers[:n]
         after = numbers[n+1:]
         
         number = numbers[n]
-        rule = rules.get(str(number), deepcopy(default))
+        rule = rules.get(str(number), default)
         
         # The most confusing line ever written in software history
         if any([a not in rule.get("before") for a in after]) or any([b not in rule.get("after") for b in before]):
-            valid = False
-            
+            return False, n
+    
+    return True, 0
+    
+p1 = 0
+invalid = []
+for i in instructions:
+    numbers = [int(j) for j in i.split(',')]
+    
+    valid, n = validate_rules(numbers, rules)
+    
     if valid:
         p1 += numbers[int(len(numbers)/2)]
+    else:
+        invalid.append((numbers, n))
         
 print(p1)
+
+def swap(list, i, j): 
+    tmp = list[i]
+    list[i] = list[j]
+    list[j] = tmp
+    
+    return list
+
+def fix(numbers, n, rules):
+    number = numbers[n]
+    rule = rules.get(str(number), default)
+    
+    
+    position = 0
+    for i in range(len(numbers)):
+        if i != n and numbers[i] in rule['after']:
+            position += 1
+    
+    if position != n:
+        return swap(numbers, position, n)
+    
+    return numbers
+
+p2 = 0
+
+while len(invalid) != 0:
+    numbers, n = invalid.pop()
+    
+    fixed = fix(numbers, n, rules)
+    
+    valid, new_n = validate_rules(numbers, rules)
+    
+    if valid:
+        p2 += numbers[int(len(numbers)/2)]
+    else:
+        invalid.append((fixed, new_n))
+
+print(p2)
