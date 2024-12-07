b3ae2fd3d483c9a9bb8f6d03b2076999ccb782de
Mateus D Dadalto <mateusdadalto@gmail.com>
2024-12-07 00:41:03 -0500

    feat(2024): day 07 part 2
    Who needs dynamic programming??

--- 2024/day_07.py
+++ 2024/day_07.py
@@ -10,6 +10,7 @@ with open(path) as f:
 
 p1 = 0
 
+solved = set()
 for line in lines:
     result, numbers = line.split(':')
     result = int(result)
@@ -23,7 +24,36 @@ for line in lines:
 
         if calculation == result:
             p1 += result
+            solved.add(line)
             break
 
-print(p1)
-        
+
+print("Part 1:", p1)
+p2 = p1
+def operation(x,y, op):
+    if op == '+':
+        return x+y
+    
+    if op == '*':
+        return x*y
+    
+    return int(str(x)+str(y))
+
+for line in lines:
+    if line not in solved:
+        result, numbers = line.split(':')
+        result = int(result)
+        numbers = [int(i) for i in numbers.split()]
+
+        n_op = len(numbers) - 1
+
+        for operators in product('*+|', repeat=n_op):
+            op = iter(operators)
+            calculation = reduce(lambda x, y: operation(x,y, next(op)), numbers)
+
+            if calculation == result:
+                p2 += result
+                solved.add(line)
+                break
+
+print("Part 2:", p2)
