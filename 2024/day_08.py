e536812d7aac2f5877573c00199d6a3d91f9bad4
Mateus D Dadalto <mateusdadalto@gmail.com>
2024-12-08 01:21:38 -0500

    refactor(2024): remove garbage from code

--- 2024/day_08.py
+++ 2024/day_08.py
@@ -34,7 +34,7 @@ def create_antinodes_2(p1,p2, rows, cols):
     i = 1
     ax = p2[0]
     ay = p2[1]
-    while 0 <= ax < ROWS and 0 <= ay < COLS:
+    while 0 <= ax < rows and 0 <= ay < cols:
         below_p1.append((ax, ay))
         ax = p2[0] - (direction[0]*i)
         ay = p2[1] - (direction[1]*i)
@@ -44,7 +44,7 @@ def create_antinodes_2(p1,p2, rows, cols):
     j = 1
     bx = p1[0]
     by = p1[1]
-    while 0 <= bx < ROWS and 0 <= by < COLS:
+    while 0 <= bx < rows and 0 <= by < cols:
         above_p2.append((bx, by))
         bx = p1[0] + (direction[0]*j)
         by = p1[1] + (direction[1]*j)
@@ -56,13 +56,9 @@ def create_antinodes_2(p1,p2, rows, cols):
 seen = []
 antinodes = set()
 antinodes_2 = set()
-pairs = 0
-out = 0
-_in = 0
 for k in interst_points:
     for j in interst_points:
         if k != j and lines[k[0]][k[1]] == lines[j[0]][j[1]] and set((k,j)) not in seen:
-            pairs += 1
             a1,a2 = create_antinodes(k,j)
             a_p2 = create_antinodes_2(k, j, ROWS, COLS)
             seen.append(set((k,j)))
@@ -70,33 +66,9 @@ for k in interst_points:
             # SPENT HALF AN HOUR DEBUGGING THIS FUCKER BECAUSE IT WAS <= COLS
             if 0 <= a1[0] < ROWS and 0<= a1[1] < COLS:
                 antinodes.add(a1)
-                _in += 1
-            else:
-                out += 1
             if 0 <= a2[0] < ROWS and 0<= a2[1] < COLS:
                 antinodes.add(a2)
-                _in += 1
-            else:
-                out += 1
                 
 
-print(len(antinodes))
-print(len(antinodes_2))
-# print("out", out)
-# print("in", _in)
-# print("pairs", pairs)
-# c = Counter(letters)
-# expected_p = 0
-# for i in c:
-#     expected_p += sum(range(c[i]))
-
-# print("expected", expected_p)
-
-# for row in range(ROWS):
-#     for col in range(COLS):
-#         if (row, col) in antinodes:
-#             print('#', end='')
-#         else:
-#             print(lines[row][col], end='')
-#     print()
-            
+print("p1:", len(antinodes))
+print("p2:", len(antinodes_2))
