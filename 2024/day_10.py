d30f412d67d2a858ca5fee4c93055d1b95941964
Mateus D Dadalto <mateusdadalto@gmail.com>
2024-12-10 00:22:09 -0500

    feat(2024): day 10 part 1
    Finally one without bugs
    
    I bet part 2 will bite me

---
+++ 2024/day_10.py
@@ -0,0 +1,54 @@
+from collections import deque
+
+path = "day_10.txt"
+# path = "test.txt"
+
+directions = [
+    (0, -1),
+    (1, 0),
+    (0, 1),
+    (-1, 0)
+]
+
+with open(path) as f:
+    lines = [l.strip() for l in f.readlines()]
+    
+
+ROWS = len(lines)
+COLS = len(lines[0])
+
+trail_heads = []
+for row in range(ROWS):
+    for col in range(COLS):
+        tile = lines[row][col]
+        if tile.isnumeric() and int(tile) == 0:
+            trail_heads.append((row,col))
+
+def is_inside(grid, row, col):
+    return 0 <= row < len(grid) and 0 <= col < len(grid[0])
+
+total = 0
+
+def count_score(grid, trail_head):
+    score = 0
+    visited_ends = set()
+    tiles = [trail_head]
+    
+    while len(tiles) > 0:
+        row, col = tiles.pop()
+        height = int(grid[row][col])
+        
+        if (row, col) not in visited_ends and height == 9:
+            score += 1
+            visited_ends.add((row, col))
+            continue
+        
+        for r, c in directions:
+            new_row = row + r
+            new_col = col + c
+            if is_inside(grid, new_row, new_col) and int(grid[new_row][new_col]) - height == 1:
+                tiles.append((new_row, new_col))
+            
+    return score
+
+print(sum([count_score(lines, i) for i in trail_heads]))
