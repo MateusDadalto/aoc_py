ce64127617618fca8bb573a0abbed476dd752d80
Mateus D Dadalto <mateusdadalto@gmail.com>
2024-12-20 01:53:32 -0500

    feat(2024): day 20 part 2
    I overcomplicated it, the realisation of what was wrong the comment change on line 65 of this commit

--- 2024/day_20.py
+++ 2024/day_20.py
@@ -1,5 +1,8 @@
+from functools import cache
 import heapq
 from collections import Counter
+from types import MappingProxyType
+from typing import Dict
 
 path = "day_20.txt"
 # path = "test.txt"
@@ -27,7 +30,7 @@ def is_valid(pos:complex, grid):
     
     return 0 <= r < rows and 0<= c < cols and grid[r][c] != '#'
     
-def dijkstra(grid, start: complex):
+def dijkstra(grid, start: complex): 
 
     seen = set()
     nodes = {}
@@ -51,7 +54,7 @@ def dijkstra(grid, start: complex):
     
     return nodes
 
-path = dijkstra(grid, start)
+path: Dict[complex, int] = dijkstra(grid, start)
 
 jumps = []
 total = 0
@@ -59,7 +62,7 @@ for node in path:
     for d in directions:
         leap_end = node + d*2
         if is_valid(leap_end, grid) and not is_valid(node + d, grid):
-            # minus 2 to to discount their own node time
+            # the distance between them - 2 to account the steps
             leap_size = path[leap_end] - path[node] - 2
             
             if leap_size >= 100:
@@ -68,4 +71,33 @@ for node in path:
 
 # print(Counter(jumps))
 print(total)
+# print(path.keys())
+# p2
 
+# since python dict since 3.7 maintain order of insertion
+p2_jumps = []
+p2 = 0
+keys = [k for k in path.keys()]
+for i in range(len(keys)):
+    node1 = keys[i]
+    for j in range(i, len(keys)):
+        node2 = keys[j]
+        grid_distance = node2 - node1
+        # taxicab distance
+        tb_distance = abs(grid_distance.real) + abs(grid_distance.imag)
+        path_distance = path[node2] - path[node1]
+        
+        if 0 < tb_distance <= 20 and tb_distance < path_distance:
+            leap_size = int(path_distance - tb_distance)
+            
+            if leap_size >= 100:
+                p2_jumps.append(leap_size)
+                p2 += 1
+    
+    percentage = int(i/len(keys) *100)
+    print(f'{percentage}%', end= '\r')
+
+print(f'100%', end= '\r')
+
+print(Counter(p2_jumps))
+print(p2)
