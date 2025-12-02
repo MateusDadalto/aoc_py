ef9eb624e4d4a74da80f5cce3d62ffd9dc4c2491
Mateus D Dadalto <mateusdadalto@gmail.com>
2025-12-01 21:37:50 -0500

    refactor(2025): Day 1 part 1
    Using 100% of the human brain

--- 2025/day_01.py
+++ 2025/day_01.py
@@ -8,6 +8,7 @@ operation = {
 
 part1 = 0
 counter = 0
+current = 50
 t = 50
 with open(path) as f:
     lines = f.readlines()
@@ -17,17 +18,18 @@ with open(path) as f:
         o = l[0]
         n = int(l[1:])
         
-        for _ in range(n):
-            t += operation[o]
-            
-            t = t%100
-            
-            if t == 0:
-                counter += 1
-                
-        if t == 0:
-            part1 += 1
+        times_passed = n//100
+        result = ((n%100) * operation[o]) + current
+        
+        if current != 0 and (result <= 0 or result >= 100):
+            times_passed += 1
 
+        current = result % 100
+        
+        counter += times_passed
+        
+        if current == 0:
+            part1 += 1
             
 print("Day 1 part 1: ", part1)
-print("Day 1 part 2: ", counter)
+print("Day 1 part 2: ", counter) # 6671
