b843894f47ec08a40f744ea76a0f565f89c55d2b
Mateus D Dadalto <mateusdadalto@gmail.com>
2025-12-02 21:32:28 -0500

    feat(2025): day 2 part 2
    I won't remember this solution in 3h from now

--- 2025/day_02.py
+++ 2025/day_02.py
@@ -4,7 +4,6 @@ path = "day_02.txt"
 with open(path) as f:
     ranges = [r.strip() for r in f.read().split(',')]
     
-    
 # print(ranges)
 
 def is_valid(n: int):
@@ -19,16 +18,41 @@ def is_valid(n: int):
         return False
     
     return True
+
+def is_valid_pt_2(n: int):
+    primes = [2,3,5,7,11,13,17]
+    n_str = str(n)
+    n_size = len(n_str)
     
-count = 0
+    for p in primes:
+        if n_size%p == 0 and compare_sections(n_str, p):
+            return False
+    
+    return True
+
+def compare_sections(s: str, p: int):
+    section_size = len(s)//p
+    
+    sections = set()
+    for i in range(0, len(s), section_size):
+        sections.add(s[i:i+section_size])
+    
+    # print(s, p, sections)
+    return len(sections) == 1
+    
+
+part_1 = 0
+part_2 = 0
 for s in ranges:
-    print(s)
+    # print(s)
     start,end = s.split('-')
     
     valids = [i for i in range(int(start), int(end)+1) if not is_valid(i)]
-    print(valids)
-    count += sum(valids)
+    valids_2 = [i for i in range(int(start), int(end)+1) if not is_valid_pt_2(i)]
+    # print(valids)
+    part_1 += sum(valids)
+    part_2 += sum(valids_2)
 
 
-print(count)
-    
+print("Day 2 part 1:", part_1)
+print("Day 2 part 2:", part_2)
