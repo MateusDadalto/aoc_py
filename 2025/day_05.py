50376be46cdaedaf8ef120afe7e772b68ad792cb
Mateus D Dadalto <mateusdadalto@gmail.com>
2025-12-05 21:49:38 -0500

    feat(2025) day 5 part 2

--- 2025/day_05.py
+++ 2025/day_05.py
@@ -20,3 +20,69 @@ for x in ids.split("\n"):
         
         
 print("Day 5 part 1:", counter)
+
+def merge_ranges(ranges: tuple[int,int]):
+    
+    merged_ranges: set[tuple[int, int]] = set([ranges[0]])
+    
+    for (start, end) in ranges:
+        new_ranges: set[tuple[int, int]] = set()
+        compare_start = start
+        compare_end = end
+        
+        for (merged_start, merged_end) in merged_ranges:
+            
+            # starts before and ends inside the range
+            if compare_start < merged_start and merged_start <= compare_end <= merged_end:
+                if (compare_start, compare_end) in new_ranges:
+                    new_ranges.remove((compare_start, compare_end))
+                    
+                compare_end = merged_end
+                new_ranges.add((compare_start, compare_end))
+                
+            # starts in the inside and ends after the range
+            elif merged_start <= compare_start <= merged_end and merged_end < compare_end:
+                if (compare_start, compare_end) in new_ranges:
+                    new_ranges.remove((compare_start, compare_end))
+                    
+                compare_start = merged_start
+                new_ranges.add((compare_start, compare_end))
+            
+            # range totally contained in the comparison range 
+            elif merged_start <= compare_start and compare_end <= merged_end:
+                if (compare_start, compare_end) in new_ranges:
+                    new_ranges.remove((compare_start, compare_end))
+                    
+                compare_start = merged_start
+                compare_end = merged_end
+                new_ranges.add((compare_start, compare_end))
+
+            # comparison range totally contained in range (I Forgot this consition at first)
+            elif compare_start <= merged_start and merged_end <= compare_end:
+                new_ranges.add((compare_start, compare_end))
+            
+            # range not intersecting with comparison range
+            else:
+                new_ranges.add((compare_start, compare_end))
+                new_ranges.add((merged_start, merged_end))
+        
+        merged_ranges = new_ranges
+        
+    return merged_ranges
+    
+result = list(merge_ranges(range_lines))
+print(result)
+
+# validation (this made me pick the forgotten condition)
+for i in range(len(result)):
+    start_i, end_i = result[i]
+    for j in range(len(result)):
+        start_j, end_j = result[j]
+        if i == j:
+            continue
+        
+        if start_j <= start_i <= end_j or start_j <= end_i <= end_j:
+            print("ranges not merged", (start_i, end_i), (start_j, end_j))
+        
+
+print("Day 5 part 2:", sum([(end - start) + 1 for start,end in result]))
